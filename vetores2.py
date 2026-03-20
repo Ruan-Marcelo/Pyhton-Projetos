27516ed4c60ed60d6e43bd6b5a458a54fa48b9ea
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-03-19 22:02:44 -0300

    Add files via upload

---
+++ vetores2.py
@@ -0,0 +1,9 @@
+frutas = []
+frutas.append("Laranja")
+frutas.append("Ovo")
+frutas.append("Moranga")
+frutas.append("Banana")
+frutas.sort()
+frutas.insert(0,"Bergamota")
+frutas.clear()
+print(frutas)
